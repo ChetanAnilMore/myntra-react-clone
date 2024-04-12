0a0afac6ddb96532c83c6691d80149a6be8a3eac
ChetanAnilMore <chetanmore733@gmail.com>
2024-04-12 12:58:27 +0530

    Completed basic structure of the application

---
+++ myntra-react-clone/src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="d-flex justify-content-center my-5">
+      <div
+        className="spinner-border"
+        style={{ width: "5rem", height: "5rem" }}
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
